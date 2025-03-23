9149f60f265d7113a5eccfc7ffb1540c4d85d206
chad <wsrfssx@yeah.net>
2025-03-23 23:47:24 +0800

    feat: TFT_eSPI 驱动配置成功

--- nwach/src/main.cpp
+++ nwach/src/main.cpp
@@ -1,20 +1,47 @@
 #include <Arduino.h>
 #include "bsp/power/power.h"
+#include <TFT_eSPI.h>
 
 Power power;
 
-void setup() {
-  Serial.begin(115200);
+TFT_eSPI tft = TFT_eSPI(200,200);
+#define BL_PIN GPIO_NUM_42  // 替换为实际背光引脚
+
+void display(){
+  tft.init();
+  tft.fillScreen(TFT_LIGHTGREY);
+  //pinMode(BL_PIN, OUTPUT);
   
-  power.init();
+
+}
+
+void setup() {
+  //Serial.begin(115200);
+  //delay(1000);
+  //Serial.printf("test port1");
+  //pinMode(BL_PIN, OUTPUT);
+  //gpio_set_direction(BL_PIN, GPIO_MODE_OUTPUT_OD);
+
+  
+  //power.init();
+/*
+  Serial.printf("test port2");
+  if (psramFound()) {
+    Serial.println("PSRAM detected!（但硬件没有PSRAM，说明配置错误）");
+  } else {
+    Serial.println("No PSRAM（正确状态）");
+  }*/
+  display();
 }
 
 // the loop function runs over and over again forever
 void loop() {
+
+
   // power test
   #if 0
   Serial.printf("volt: %.2f (%d%%)\n", power.readBatVoltage(), power.readBatPercentage());
   power.keepAlive();
-  delay(10000);
+  delay(1000);
   #endif
 }
